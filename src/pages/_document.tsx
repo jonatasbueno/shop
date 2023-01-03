a8bb77a81528254a08971da22d950c11c3718da7
Fluency Academy <fluencyacademy@MacBook-Air-de-Fluency-3.local>
2023-01-03 02:26:59 -0300

    feat: Structure update (created src folder)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,7 @@
  * - Hot Relouad não se aplica a esse arquivo, logo a acada alteração deve-se reiniciar o projeto com "npm run ..."
  */}
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles'
 
 export default function Document() {
   return (
@@ -17,6 +18,9 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+
+        {/** Indica que a aplicação de estilos do stiches deve ser SERVER SIDE */}
+        <style id='stitches' dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
         <Main />
