ea3fa96a1ee7d6f7d8318f3a03e0111286c65952
Fluency Academy <fluencyacademy@MacBook-Air-de-Fluency-3.local>
2023-01-03 22:29:49 -0300

    feat: Created Stiches styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,18 @@ import type { AppProps } from 'next/app'
 
 import { globalStyles } from '../styles/global';
 
+import logoImage from "../assets/logo.svg"
+import { Container, Header } from '../styles/pages/app';
+
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImage.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
