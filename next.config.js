d1e6a322af6bb859047b02fa2bab7a177eab1dd9
Fluency Academy <fluencyacademy@MacBook-Air-de-Fluency-3.local>
2023-01-06 20:21:07 -0300

    feat: SSG implemented

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  /** Definindo dominios que o next irá tratar as imagens */
+  images: {
+    domains: [
+      'files.stripe.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
+import { GetStaticProps } from 'next';
 import Image from 'next/image'
 import { useKeenSlider } from "keen-slider/react"
+import Stripe from 'stripe';
 
+import { stripe } from '../lib/stripe';
 import { HomeContainer, Product } from "../styles/pages/Home";
 
-import camiseta1 from "../assets/camisetas/Shirt-1.png"
-import camiseta2 from "../assets/camisetas/Shirt-2.png"
-import camiseta3 from "../assets/camisetas/Shirt-3.png"
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: number
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -17,37 +26,56 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta1} width={520} height={400} alt='' priority />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+      {products.map(product => (
+        <Product key={product.id} className='keen-slider__slide'>
+          <Image src={product.imageUrl} width={520} height={400} alt='' priority />
+          <footer>
+            <strong>{product.name}</strong>
+            <span>{product.price}</span>
+          </footer>
+        </Product>
+      ))}
 
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta2} width={520} height={400} alt='' priority />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta2} width={520} height={400} alt='' priority />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className='keen-slider__slide'>
-        <Image src={camiseta2} width={520} height={400} alt='' priority />
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
     </HomeContainer>
   )
+}
+
+
+/** 
+ * o método getServerSideProps irá ser executado no Node Server a cada request 
+ * feito para carregar a página, afim de injetar propriedades para a page 
+ * requisitada
+ */
+
+
+/**
+ * o método getStaticProps ir[a er executado no momento do build e criará páginas
+ * integralmente staticas e ficaram armazenadas numa CDN até que seu tempo limite
+ * seja excedido (tempo definito no parametro 'revalidate' no retorno do método)
+ */
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    /** Necessário nessa api para aplicação de expand em seu response */
+    expand: ['data.default_price']
+  })
+
+  const products = response.data.map(product => {
+    /** Forçando type de Price no parametro default_price */
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      /** os preços são salvos em centavos (uma boa prática pra evitar problemas de vírgula) */
+      price: price?.unit_amount ? price.unit_amount / 100 : 0
+    }
+  })
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 60 * 60 * 2 /** 2 horas */
+  }
 }
